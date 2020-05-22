46239cd8ff3bc5a61d2641f1ccb1921524cf3997
Fazle Rabbi Linkon <fazle.linkon@dreamonline.co.jp>
2020-05-22 11:27:32 +0600

    Initial Commit

---
+++ Demo Speech to text/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo Speech to text
+//
+//  Created by Fazle Rabbi Linkon on 22/5/20.
+//  Copyright © 2020 DreamOnline. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Demo Speech to textTests/Demo_Speech_to_textTests.swift
@@ -0,0 +1,34 @@
+//
+//  Demo_Speech_to_textTests.swift
+//  Demo Speech to textTests
+//
+//  Created by Fazle Rabbi Linkon on 22/5/20.
+//  Copyright © 2020 DreamOnline. All rights reserved.
+//
+
+import XCTest
+@testable import Demo_Speech_to_text
+
+class Demo_Speech_to_textTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
